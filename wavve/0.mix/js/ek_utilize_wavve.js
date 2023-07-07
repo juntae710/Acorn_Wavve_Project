9515c5ba78c6d8c32ee8faff43635b3e7163c3b9
juntae710 <pjt010914@gmail.com>
2023-07-07 11:39:03 +0900

    index

---
+++ wavve/0.mix/js/ek_utilize_wavve.js
@@ -0,0 +1,14 @@
+let flag = true;
+        function btn_click(){
+            let a = document.getElementById("page_voucher_banner");
+            let b = document.getElementById("page_voucher_btn");
+            if(flag==true){
+                a.style.height = "auto";
+                b.style.transform = "rotate(180deg)";
+                flag = false;
+            }else{
+                a.style.height = "110px";
+                b.style.transform = "rotate(0deg)";
+                flag = true;
+            }
+        }
